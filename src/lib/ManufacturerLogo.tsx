edb986e3f3e2a1f684cf9174df3c1e2b2773079a
Charles Pick <charles@codemix.com>
2024-03-20 00:07:40 +0000

    Add logos B to D

--- src/lib/ManufacturerLogo.tsx
+++ src/lib/ManufacturerLogo.tsx
@@ -8,7 +8,20 @@ import { AmagLogo } from './manufacturers/AmagLogo.js'
 import { AmericanStandardLogo } from './manufacturers/AmericanStandardLogo.js'
 import { AprilAireLogo } from './manufacturers/AprilAireLogo.js'
 import { AqaraLogo } from './manufacturers/AqaraLogo.js'
+import { AssaLogo } from './manufacturers/AssaLogo.js'
+import { AugustLogo } from './manufacturers/AugustLogo.js'
+import { AvigilonLogo } from './manufacturers/AvigilonLogo.js'
+import { BoschLogo } from './manufacturers/BoschLogo.js'
+import { BrivoLogo } from './manufacturers/BrivoLogo.js'
+import { ButterflyMXLogo } from './manufacturers/ButterflyMXLogo.js'
+import { CCureLogo } from './manufacturers/CCureLogo.js'
+import { ChamberlainLogo } from './manufacturers/ChamberlainLogo.js'
+import { ComelitLogo } from './manufacturers/ComelitLogo.js'
 import type { CommonManufacturerLogoProps } from './manufacturers/CommonManufacturerLogoProps.js'
+import { ControlByWebLogo } from './manufacturers/ControlByWebLogo.js'
+import { DoorKingLogo } from './manufacturers/DoorKingLogo.js'
+import { DoorbirdLogo } from './manufacturers/DoorbirdLogo.js'
+import { DormakabaLogo } from './manufacturers/Dormakaba.js'
 
 /**
  * A dictionary of manufacturer names to their respective logos.
@@ -24,6 +37,20 @@ export const manufacturerLogos = {
   american_standard: AmericanStandardLogo,
   aprilaire: AprilAireLogo,
   aqara: AqaraLogo,
+  assa_abloy_credential_service: null,
+  assa: AssaLogo,
+  august: AugustLogo,
+  avigilon_alta: AvigilonLogo,
+  bosch: BoschLogo,
+  brivo: BrivoLogo,
+  butterflymx: ButterflyMXLogo,
+  c_cure: CCureLogo,
+  chamberlain: ChamberlainLogo,
+  comelit: ComelitLogo,
+  controlbyweb: ControlByWebLogo,
+  doorbird: DoorbirdLogo,
+  doorking: DoorKingLogo,
+  dormakaba: DormakabaLogo,
 } as const
 
 type ManufacturerName = keyof typeof manufacturerLogos
